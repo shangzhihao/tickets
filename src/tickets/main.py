5dec13bd4eb98f6136ba7bdd86c9438a7d72cb05
Shang Zhihao <zhihao.shang@gmail.com>
2025-10-03 23:57:37 +0200

    configure loger

--- src/tickets/main.py
+++ src/tickets/main.py
@@ -1,11 +1,11 @@
 # src/ml_project/train.py
 import hydra
 from omegaconf import DictConfig
-
+from .utils.logger import logger
 @hydra.main(config_path="../../conf", config_name="config", version_base=None)
 def main(cfg: DictConfig):
-    print(cfg.seed)
-    print(cfg.project_name)
+    logger.info(cfg.seed)
+    logger.info(cfg.project_name)
 
 if __name__ == "__main__":
     main()
---
+++ src/tickets/utils/logger.py
@@ -0,0 +1,13 @@
+from loguru import logger
+logger.remove()
+logger.add(
+    "logs/app.log",
+    serialize=True,
+    enqueue=True,
+    rotation="10 MB",
+    retention="7 days",
+    compression="zip"
+)
+logger.add(lambda msg: print(msg, end=""))
+# Export the logger for reuse
+__all__ = ["logger"]
